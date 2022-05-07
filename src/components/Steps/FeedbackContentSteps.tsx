a5d63167b68e9d3e2ac17d6a21c155fe5c50ef32
Jean carlos <jeandub1@hotmail.com>
2022-05-06 21:45:47 -0400

    finish form

--- src/components/Steps/FeedbackContentSteps.tsx
+++ src/components/Steps/FeedbackContentSteps.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft, Camera } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { CloseButton } from "../CloseButton";
 import { FeedBackType, feedbackTypes } from "../WidgetForm";
 import { ScreenShotButton } from "../WidgetForm/ScreenShotButton";
@@ -15,6 +15,20 @@ function FeedbackContentSteps({
 }: FeedBackStepProps) {
     const feedbackTypeInfo = feedbackTypes[feedBackType];
     const [screenshot, setScreenshot] = useState<string | null>(null);
+    const [comment, setComment] = useState('');
+
+
+    function handleSubmitFeedBack(event: FormEvent) {
+        event.preventDefault()
+
+        console.log({
+            screenshot,
+            comment
+        });
+
+
+    }
+
     return (
         <>
             <header>
@@ -36,10 +50,11 @@ function FeedbackContentSteps({
                 <CloseButton />
             </header>
 
-            <form action=" my-4 w-full">
+            <form action=" my-4 w-full" onSubmit={handleSubmitFeedBack}>
                 <textarea
                     className="min-w-[304px] w-full min-h-[112px] text-sm placeholder:text-zinc-400 text-zinc-100  border-zinc-600 bg-transparent focus:outline-none rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none scrollbar-thumb-zinc-400 scrollbar-track-transparent scrollbar-thin"
                     placeholder="Conte o que está acontecendo..."
+                    onChange={event => setComment(event.target.value)}
                 ></textarea>
 
                 <footer className="flex mt-2  gap-2">
@@ -50,7 +65,8 @@ function FeedbackContentSteps({
 
                     <button
                         type="submit"
-                        className=" transition-colors  p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 "
+                        disabled={comment.length === 0}
+                        className=" disabled:opacity-50 disabled:hover:bg-brand-500 transition-colors  p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 "
                     >
                         <p>Enviar FeedBack</p>
                     </button>
